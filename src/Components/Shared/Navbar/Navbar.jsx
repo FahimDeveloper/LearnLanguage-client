84062445c400b2dc7b7bea4bbce0f6408e20bd8b
FahimDeveloper <developerfahim246@gmail.com>
2023-06-12 00:19:03 +0600

    fix banner and create navbar with navigation

--- src/Components/Shared/Navbar/Navbar.jsx
+++ src/Components/Shared/Navbar/Navbar.jsx
@@ -1,31 +1,22 @@
-import { } from 'react';
 import { NavLink } from 'react-router-dom';
+import { HiShoppingCart } from "react-icons/hi2";
 
 const Navbar = () => {
     return (
-        <div className="navbar bg-base-100">
-            <div className="navbar-start">
-                <div className="dropdown">
-                    <label tabIndex={0} className="btn btn-ghost lg:hidden">
-                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
-                    </label>
-                    <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-                        <NavLink to="/">Home</NavLink>
-                        <NavLink to="/instructors">Instructors</NavLink>
-                        <NavLink to="/allClasses">Classes</NavLink>
-                    </ul>
+        <div className='bg-base-100 py-5'>
+            <div className='container mx-auto flex items-center justify-between'>
+                <div>
+                    <h3 className='text-3xl uppercase font-bold italic'>learn language</h3>
                 </div>
-                <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
-            </div>
-            <div className="navbar-center hidden lg:flex">
-                <ul className="menu menu-horizontal px-1">
+                <div className='text-xl font-medium flex items-center gap-5'>
                     <NavLink to="/">Home</NavLink>
                     <NavLink to="/instructors">Instructors</NavLink>
                     <NavLink to="/allClasses">Classes</NavLink>
-                </ul>
-            </div>
-            <div className="navbar-end">
-                <a className="btn">Button</a>
+                    <NavLink to="/allClasses"><HiShoppingCart className='text-3xl' /></NavLink>
+                </div>
+                <div>
+                    <button className='btn btn-primary px-10'>login</button>
+                </div>
             </div>
         </div>
     );
--- src/Pages/Client/Home/Components/BannerSection/BannerSection.jsx
+++ src/Pages/Client/Home/Components/BannerSection/BannerSection.jsx
@@ -8,11 +8,13 @@ import banner3 from "../../../../../assets/images/banner3.jpg"
 
 const BannerSection = () => {
     return (
-        <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
-            <SwiperSlide><img className="w-full h-[800px] object-cover" src={banner1} alt="banner image" /></SwiperSlide>
-            <SwiperSlide><img className="w-full h-[800px] object-cover" src={banner2} alt="banner image" /></SwiperSlide>
-            <SwiperSlide><img className="w-full h-[800px] object-cover" src={banner3} alt="banner image" /></SwiperSlide>
-        </Swiper>
+        <div className="container mx-auto py-5">
+            <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
+                <SwiperSlide><img className="w-full h-[800px] object-contain" src={banner1} alt="banner image" /></SwiperSlide>
+                <SwiperSlide><img className="w-full h-[800px] object-contain" src={banner2} alt="banner image" /></SwiperSlide>
+                <SwiperSlide><img className="w-full h-[800px] object-contain" src={banner3} alt="banner image" /></SwiperSlide>
+            </Swiper>
+        </div>
     );
 };
 
